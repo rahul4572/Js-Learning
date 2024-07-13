dc5de170642e9414b7cb1ba1f794e7fed98821f5
Rahul_Bhatt <123260283+rahul4572@users.noreply.github.com>
2024-07-13 08:05:09 +0000

    dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 13556
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "14525"
+accountCity = "Jaipur"
+
+accountEmail = "bhayy@gmail.com"
+accountPassword = "789945"
+accountCity = "Dehradun"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* prefer not to use var
+bcoz of issue in block scope and functional scope
+*/
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3)  // we are using node.js, not browser
+
+console.log("Hey")
+console.log("Mr.Bhatt")
+
+// number => 2 to power 53 in memory address
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => for uniqueness specially in react
+
+// object 
+
+console.log(typeof undefined)
+
+  
